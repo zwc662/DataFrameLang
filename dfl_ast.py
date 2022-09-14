dbfe865121375f1db534741e893e0144c0a6dbd5
zwc662 <zwc662@gmail.com>
2022-09-14 15:59:41 -0400

    update

--- dfl_ast.py
+++ dfl_ast.py
@@ -1,12 +1,22 @@
 from abc import ABC, abstractmethod
 import enum
+from types import NotImplementedType
 
 class DFL_AST_TOKEN(enum.Enum):
+    EXIST = 0
     BASE = 1
-    
+    LIST = 2
+    FUNCTION = 3
+    TUPLE = 4
 
+    @classmethod
+    def from_name(cls, name):
+        for model in cls.model:
+            if cls.model.name == name:
+                return cls.model
+        raise NotImplementedType
 
-class _DFL_AST_NODE(ABC):
+class DFL_AST_NODE(ABC):
     def __init__(self, token, *value):
         self._token = token
         self._value = value
@@ -19,5 +29,14 @@ class _DFL_AST_NODE(ABC):
     def value(self):
         return self._value
 
-    @value.setattr
-    def value 
+    @value.setter 
+    def value(self, *args, **kwargs):
+        raise NotImplementedError
+    
+    @token.setter
+    def token(self, *args, **kwargs):
+        raise NotImplementedError
+    
+
+class DFL_BASE(DFL_AST_NODE):
+                                                               
