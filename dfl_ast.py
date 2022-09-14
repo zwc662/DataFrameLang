b71fbdf0b824180c072dd5795d1119eb76110804
superradiance <zwc662@gmail.com>
2022-09-14 15:28:22 -0400

    update

--- dfl_ast.py
+++ dfl_ast.py
@@ -2,8 +2,12 @@ from abc import ABC, abstractmethod
 import enum
 
 class DFL_AST_TOKEN(enum.Enum):
+    EXIST = 0
     BASE = 1
-    
+    DATAFRAME = 2
+    LIST = 3
+    TUPLE = 4
+    FUNCTION = 5
 
 
 class _DFL_AST_NODE(ABC):
